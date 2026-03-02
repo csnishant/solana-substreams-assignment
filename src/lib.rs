2de0675bbc7d6b895c79f56853de8cfdc1fb19ae
Nishant Choudhary <148760278+csnishant@users.noreply.github.com>
2026-03-02 09:56:22 +0000

    feat: initialize Rust project for Substreams module

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,64 @@
 #[allow(unused)]
 mod pb;
+
 use pb::mydata::v1 as mydata;
-use substreams_solana::b58;
+
 use substreams_solana::pb::sf::solana::r#type::v1::Block;
+use substreams_solana::b58;
 
-// Get the program id for the pump fun program as byte array for fast comparison
-// If you change this for another program, be sure to check substreams.yaml and:
-// * If you are using block filters update the filter accordingly
-// * Align the module's initialBlock with the creation of your program
-
-const PUMP_FUN_PROGRAM_ID: [u8; 32] = b58!("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P");
+// Solana System Program ID (native SOL transfers)
+const SYSTEM_PROGRAM_ID: [u8; 32] =
+    b58!("11111111111111111111111111111111");
 
 #[substreams::handlers::map]
-fn map_my_data(blk: Block) -> mydata::MyData {
-    let mut account_vec: Vec<Vec<u8>> = Vec::new();
+fn map_my_data(blk: Block) -> mydata::Transfers {
+    let mut transfers = mydata::Transfers { transfers: vec![] };
 
-    blk.walk_instructions()
-        .filter(|instruction| instruction.program_id() == PUMP_FUN_PROGRAM_ID)
-        .for_each(|instruction| {
-            instruction.accounts().into_iter().for_each(|account| {
-                account_vec.push(account.0.clone());
+    for trx in blk.transactions() {
+        for instruction in trx.walk_instructions() {
+
+            // Only system program
+            if instruction.program_id() != SYSTEM_PROGRAM_ID {
+                continue;
+            }
+
+            let accounts = instruction.accounts();
+            if accounts.len() < 2 {
+                continue;
+            }
+
+            // System transfer instruction = 4 bytes discriminator + 8 bytes amount
+            let data = instruction.data();
+            if data.len() < 12 {
+                continue;
+            }
+
+            // First 4 bytes = instruction type
+            let instruction_type = u32::from_le_bytes([
+                data[0], data[1], data[2], data[3]
+            ]);
+
+            // 2 = SystemProgram::Transfer
+            if instruction_type != 2 {
+                continue;
+            }
+
+            // Next 8 bytes = amount
+            let amount = u64::from_le_bytes([
+                data[4], data[5], data[6], data[7],
+                data[8], data[9], data[10], data[11],
+            ]);
+
+            let from = bs58::encode(accounts[0].0.clone()).into_string();
+            let to = bs58::encode(accounts[1].0.clone()).into_string();
+
+            transfers.transfers.push(mydata::Transfer {
+                from,
+                to,
+                amount,
             });
-        });
+        }
+    }
 
-    account_vec.sort();
-    account_vec.dedup();
-
-    let mut my_data = mydata::MyData::default();
-    my_data.accounts = account_vec;
-    my_data
+    transfers
 }
